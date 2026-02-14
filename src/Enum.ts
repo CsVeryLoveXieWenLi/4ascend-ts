14e048902c2acbd95ed241db5aca469374d526cc
CsVeryLoveXieWenLi <csvlxwl@163.com>
2026-02-14 16:45:10 +0800

    feat(enum): 添加棋子状态

---
+++ src/Enum.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: CsVeryLoveXieWenLi
+ * @Description: 枚举
+ * @Copyright (c) 2026 by CsVeryLoveXieWenLi, All Rights Reserved.
+ */
+export enum STONE_ENUM {
+    NONE,
+
+    BLACK,
+    WHITE,
+
+    BLACK_ASCEND,
+    WHITE_ASCEND,
+}

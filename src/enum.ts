1bf772876e9be69168a4789c007937c0b1e7cfd9
CsVeryLoveXieWenLi <csvlxwl@163.com>
2026-02-18 19:12:38 +0800

    feat(enum): 增加棋子和植物

---
+++ src/enum.ts
@@ -0,0 +1,21 @@
+/*
+ * @Author: CsVeryLoveXieWenLi
+ * @Description: 枚举
+ * @Copyright (c) 2026 by CsVeryLoveXieWenLi, All Rights Reserved.
+ */
+export enum STONE_ENUM {
+    NONE,
+
+    BLACK,
+    WHITE,
+
+    BLACK_ASCEND,
+    WHITE_ASCEND,
+};
+
+
+export enum PLANT_ENUM {
+    NONE,
+    ONE,
+    TWO,
+};

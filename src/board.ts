21406ba42cb8a875b2a6f87f2cf17f84fd5af834
CsVeryLoveXieWenLi <csvlxwl@163.com>
2026-02-18 19:25:51 +0800

    feat(board): 一维转二维和二维转一维

--- src/board.ts
+++ src/board.ts
@@ -19,6 +19,17 @@ class Board {
 
         this.data = [];
     };
+
+
+    public index2xy(i: number): [number, number] {
+        const x = i % this.w;
+        const y = Math.floor(i / this.w);
+        return [x, y];
+    };
+
+    public xy2index(x: number, y: number): number {
+        return y * this.w + x;
+    };
 }
 
 

4759e7dd18f1a1e7f70ff7718e2d4f300fbfde95
CsVeryLoveXieWenLi <csvlxwl@163.com>
2026-02-18 19:07:21 +0800

    feat(state): 增加回合数

---
+++ src/state.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: CsVeryLoveXieWenLi
+ * @Description: 共享状态与数据
+ * @Copyright (c) 2026 by CsVeryLoveXieWenLi, All Rights Reserved.
+ */
+class State {
+    public turn_count: number = 0;
+}
+
+
+export { State };

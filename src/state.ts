6830880e91e9aa5d6c8c909b32fa2ec1f90ce712
CsVeryLoveXieWenLi <csvlxwl@163.com>
2026-02-20 22:51:07 +0800

    feat(state): 新增turn_ascend

--- src/state.ts
+++ src/state.ts
@@ -11,6 +11,7 @@ class State {
     public turn_player: PLAYER_ENUM = PLAYER_ENUM.BLACK;
 
     public turn_atk: boolean = false;
+    public turn_ascend: boolean = false; // 进入ASCEND状态，然后对消，变为不进入
 
     public hp: [number, number] = [6, 6];
 }
